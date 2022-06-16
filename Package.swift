bedd90cd760f4f879b13cb557f7ee381c1c4e139
Marie Top Mygind <marietopmygind@gmail.com>
2022-06-16 20:16:25 +0200

    Changed package name to match git repos

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "AsyncBle",
+    name: "AsyncBluetooth",
     platforms: [.iOS(.v13)],
     products: [
         .library(name: "CoreBluetoothWrapper", targets: ["CoreBluetoothWrapper"]),
